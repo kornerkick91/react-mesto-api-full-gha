b8bd273b7d88114541aa39f644893e465382d7e9
Maksim Zakharchuk <mad_max0100@mail.ru>
2023-05-07 14:46:36 +0300

    добавить cors и logger

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const sign = require('./routes/auth');
+const cors = require('./middlewares/cors');
 const auth = require('./middlewares/auth');
 const centralErrorsHandler = require('./middlewares/centralErrorsHandler');
 const router = require('./routes');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { NotFoundError } = require('./errors');
 
 const { PORT = 3000 } = process.env;
@@ -16,16 +18,26 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+app.use(cors);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(sign);
 app.use(auth);
 app.use(router);
-
-app.use(errors());
 
 app.use((req, res, next) => {
   const err = new NotFoundError('Был запрошен несуществующий роут.');
   next(err);
 });
+
+app.use(errorLogger);
+app.use(errors());
 
 app.use(centralErrorsHandler);
 
