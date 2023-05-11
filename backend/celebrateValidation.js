0606af2280c4e26b24840f10068470671185cc9b
Maksim Zakharchuk <mad_max0100@mail.ru>
2023-05-11 12:46:54 +0300

    fix: исправить валидацию 2

--- backend/celebrateValidation.js
+++ backend/celebrateValidation.js
@@ -3,28 +3,31 @@ const { urlRegex } = require('./utils/constants');
 
 const getUserByIdValidation = {
   params: Joi.object({
-    userId: Joi.string().hex().length(24).message('Введен некорректный id.')
+    userId: Joi.string().required().hex().length(24)
+      .message('Введен некорректный id.')
   })
 };
 
 const updateProfileValidation = {
   body: Joi.object({
-    name: Joi.string().min(2).max(30).messages({
-      'string.min': 'Поле "Имя" должно содержать не менee 2 символов.',
-      'string.max': 'Поле "Имя" должно содержать не болee 30 символов.',
-      'any.required': 'Поле "Имя" должно быть заполнено.'
-    }),
-    about: Joi.string().min(2).max(30).messages({
-      'string.min': 'Поле "Род деятельности" должно содержать не менее 2 символов.',
-      'string.max': 'Поле "Род деятельности" должно содержать не более 30 символов.',
-      'any.required': 'Поле "Род деятельности" должно быть заполнено.'
-    })
+    name: Joi.string().required().min(2).max(30)
+      .messages({
+        'string.min': 'Поле "Имя" должно содержать не менee 2 символов.',
+        'string.max': 'Поле "Имя" должно содержать не болee 30 символов.',
+        'any.required': 'Поле "Имя" должно быть заполнено.'
+      }),
+    about: Joi.string().required().min(2).max(30)
+      .messages({
+        'string.min': 'Поле "Род деятельности" должно содержать не менее 2 символов.',
+        'string.max': 'Поле "Род деятельности" должно содержать не более 30 символов.',
+        'any.required': 'Поле "Род деятельности" должно быть заполнено.'
+      })
   })
 };
 
 const updateAvatarValidation = {
   body: Joi.object({
-    avatar: Joi.string().regex(urlRegex).message('Введена некорректная ссылка.')
+    avatar: Joi.string().required().regex(urlRegex).message('Введена некорректная ссылка.')
   })
 };
 
@@ -51,17 +54,19 @@ const createCardValidation = {
 
 const deleteCardValidation = {
   params: Joi.object({
-    cardId: Joi.string().hex().length(24).messages({
-      'string.hex': 'Введен некорректный id.'
-    }),
+    cardId: Joi.string().required().hex().length(24)
+      .messages({
+        'string.hex': 'Введен некорректный id.'
+      }),
   })
 };
 
 const likeCardValidation = {
   params: Joi.object({
-    cardId: Joi.string().hex().length(24).messages({
-      'string.hex': 'Введен некорректный id.'
-    }),
+    cardId: Joi.string().required().hex().length(24)
+      .messages({
+        'string.hex': 'Введен некорректный id.'
+      }),
   })
 };
 
@@ -88,16 +93,18 @@ const signupValidation = {
       'string.password': 'Пароль должен содержать не менее 8 символов.',
       'any.required': 'Поле "Пароль" должно быть заполнено.'
     }),
-    name: Joi.string().min(2).max(30).messages({
-      'string.min': 'Поле "Имя" должно содержать не менee 2 символов.',
-      'string.max': 'Поле "Имя" должно содержать не болee 30 символов.',
-      'any.required': 'Поле "Имя" должно быть заполнено.'
-    }),
-    about: Joi.string().min(2).max(30).messages({
-      'string.min': 'Поле "Род деятельности" должно содержать не менее 2 символов.',
-      'string.max': 'Поле "Род деятельности" должно содержать не более 30 символов.',
-      'any.required': 'Поле "Род деятельности" должно быть заполнено.'
-    }),
+    name: Joi.string().required().min(2).max(30)
+      .messages({
+        'string.min': 'Поле "Имя" должно содержать не менee 2 символов.',
+        'string.max': 'Поле "Имя" должно содержать не болee 30 символов.',
+        'any.required': 'Поле "Имя" должно быть заполнено.'
+      }),
+    about: Joi.string().required().min(2).max(30)
+      .messages({
+        'string.min': 'Поле "Род деятельности" должно содержать не менее 2 символов.',
+        'string.max': 'Поле "Род деятельности" должно содержать не более 30 символов.',
+        'any.required': 'Поле "Род деятельности" должно быть заполнено.'
+      }),
     avatar: Joi.string().regex(urlRegex).message('Введена некорректная ссылка.')
   })
 };
